770546152db99f010ab7db2a5e78d0be3bcce1d7
icyveins7 <icyveins@hotmail.com>
2024-08-16 18:43:04 +0800

    enable c++20, refactor to use std::source_location
    decided on using secondary, private class in order to exploit the
    templates properly and handle the variadic arguments

--- example/log.cpp
+++ example/log.cpp
@@ -4,9 +4,15 @@ int main()
 {
   spf::Logger logger("test.log");
 
-  logger.log_debug("testing %.10f", 3.14);
-  logger.log_debug("testing %4d, %.3g", 1234, 1.23e-3);
+  logger().debug("testing %.10f", 3.14);
+  logger().debug("testing %4d, %.3g", 1234, 1.23e-3);
 
+  logger().info("testing %4s", "just checking");
+  logger().error("oh no %4d, %.3g", 12345, 1.23e6);
+
+  // You cannot do this, for good reason!
+  // FILE *fe;
+  // spf::PrivateLogWriter(std::source_location::current(), fe, fe);
 
   return 0;
 }
--- include/spfLogger.h
+++ include/spfLogger.h
@@ -1,14 +1,145 @@
 #pragma once
 
-#include <iostream>
 #include <cstdio>
+#include <ctime>
+#include <iostream>
+#include <source_location>
 #include <string>
 #include <stdexcept>
-#include <ctime>
 
 namespace spf
 {
 
+// Forward declaration
+class Logger;
+
+/**
+ * @class PrivateLogWriter
+ * @brief This has no public constructor, as intended.
+ * Instantiate the `Logger` class for actual usage;
+ * this class is automatically initialized by `Logger`,
+ * and is only used for its message-type methods:
+ * debug(), info(), and error().
+ */
+class PrivateLogWriter
+{
+  friend class Logger;
+public:
+  /**
+   * @brief Logs something for debugging
+   *
+   * @param fmt A character array string, like in printf.
+   * @param args Variable substitutions for the format specifiers in the string.
+   */
+  template <typename... Args>
+  void debug(
+    const char* fmt,
+    Args&&... args)
+  {
+    log("DEBUG", fmt, args...);
+  }
+
+  /**
+   * @brief Logs something informational
+   *
+   * @param fmt A character array string, like in printf.
+   * @param args Variable substitutions for the format specifiers in the string.
+   */
+  template <typename... Args>
+  void info(
+    const char* fmt,
+    Args&&... args)
+  {
+    log("INFO", fmt, args...);
+  }
+
+  /**
+   * @brief Logs an error message
+   *
+   * @param fmt A character array string, like in printf.
+   * @param args Variable substitutions for the format specifiers in the string.
+   */
+  template <typename... Args>
+  void error(
+    const char* fmt,
+    Args&&... args)
+  {
+    log("ERROR", fmt, args...);
+  }
+
+private:
+  std::source_location m_loc;
+  FILE* m_fp;
+  FILE* m_printstream;
+  // TODO: this is not particularly safe, since you can construct this
+  // and hold the pointer after it's destroyed?
+
+  // Private constructor
+  PrivateLogWriter(
+    std::source_location loc,
+    FILE* fp,
+    FILE* printstream
+  ) : m_loc(std::move(loc)),
+  m_fp(fp),
+  m_printstream(printstream)
+  {
+  }
+
+  /**
+   * @brief Primary handler for all log method types.
+   * Is called by those methods.
+   *
+   * @param prefix Additional prefix like 'DEBUG' or 'ERROR'.
+   * @param fmt Main format string.
+   * @param args Variable arguments for format specifier substitutions.
+   */
+  template <typename... Args>
+  void log(
+    const char* prefix,
+    const char* fmt,
+    Args... args)
+  {
+    datetime_formatter();
+    fprintf(m_printstream, "%s ", prefix);
+    fprintf(m_printstream, "(%s: %d) ",
+            m_loc.file_name(), m_loc.line());
+    fprintf(m_printstream, fmt, args...);
+    fprintf(m_printstream, "\n");
+
+    if (m_fp != nullptr)
+    {
+      fprintf(m_fp, "%s ", prefix);
+      fprintf(m_fp, "(%s: %d) ",
+              m_loc.file_name(), m_loc.line());
+      fprintf(m_fp, fmt, args...);
+      fprintf(m_fp, "\n");
+    }
+  }
+
+  /**
+   * @brief This is used to format the current date and time,
+   * and write them to both streams. Generally meant to only
+   * be called by log().
+   */
+  void datetime_formatter() {
+    std::time_t t = std::time({});
+    char timeString[std::size("yyyy-mm-ddThh:mm:ssZ")];
+    std::strftime(std::data(timeString), std::size(timeString),
+                  "%FT%TZ", std::gmtime(&t));
+
+    fprintf(m_printstream, "%s ", timeString);
+    if (m_fp != nullptr)
+      fprintf(m_fp, "%s ", timeString);
+  }
+
+
+};
+
+/**
+ * @class Logger
+ * @brief This is the class you should instantiate.
+ * It handles the safe opening/closing of the log file.
+ */
 class Logger
 {
 public:
@@ -50,10 +181,13 @@ public:
     m_printstream = stream;
   }
 
-  template <typename... Args>
-  void log_debug(const char* fmt, Args... args) {
-    log("DEBUG", fmt, args...);
+  PrivateLogWriter operator()(
+    std::source_location loc=std::source_location::current()
+  ) const
+  {
+    return PrivateLogWriter(loc, m_fp, m_printstream);
   }
+
 
 protected:
   std::string m_logfilepath = "";
@@ -76,35 +210,6 @@ protected:
     }
   }
 
-  template <typename... Args>
-  void log(const char* prefix, const char* fmt, Args... args) {
-    datetime_formatter();
-    fprintf(m_printstream, "%s ", prefix);
-    // fprintf(m_printstream, "(%s: %d) ",
-    //         __FILE__, __LINE__);
-    fprintf(m_printstream, fmt, args...);
-    fprintf(m_printstream, "\n");
-
-    if (m_fp != nullptr)
-    {
-      fprintf(m_fp, "%s ", prefix);
-      // fprintf(m_fp, "(%s: %d) ",
-      //         __FILE__, __LINE__);
-      fprintf(m_fp, fmt, args...);
-      fprintf(m_fp, "\n");
-    }
-  }
-
-  void datetime_formatter() {
-    std::time_t t = std::time({});
-    char timeString[std::size("yyyy-mm-ddThh:mm:ssZ")];
-    std::strftime(std::data(timeString), std::size(timeString),
-                  "%FT%TZ", std::gmtime(&t));
-
-    fprintf(m_printstream, "%s ", timeString);
-    if (m_fp != nullptr)
-      fprintf(m_fp, "%s ", timeString);
-  }
 };
 
 
